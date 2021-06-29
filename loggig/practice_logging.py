cc9afe8ae385d4d25ed5fbf0ae125a88145cec79
Sieye <85058109+Sieye-R@users.noreply.github.com>
2021-06-29 13:40:27 -0300

    Delete practice_logging.py

--- loggig/practice_logging.py
+++
@@ -1,9 +0,0 @@
-import sys
-from logger import logger
-
-from refinement_for_practice_logging import refinement
-
-if __name__ == "__main__":
-    genre = sys.argv[1]
-    logger.warning("Everything is going well.")
-    print(f"Popular {sys.argv[1]} movie in 2021 with the total ranks are {refinement(sys.argv[1])}.")
