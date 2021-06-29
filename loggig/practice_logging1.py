a6c68727149db8b9eaaff5567b60a0789c7bb0e0
Sieye <85058109+Sieye-R@users.noreply.github.com>
2021-06-29 19:08:15 -0300

    Update practice_logging1.py

--- loggig/practice_logging1.py
+++ loggig/practice_logging1.py
@@ -4,6 +4,10 @@ from logger import logger
 from refinement_for_practice_logging import refinement
 
 if __name__ == "__main__":
+    try:
     genre = sys.argv[1]
     logger.warning("Everything is going well.")
     print(f"Popular {sys.argv[1]} movie in 2021 with the total ranks are {refinement(sys.argv[1])}.")
+      
+    except BaseException:
+        logger.error("Error happened!", exc_info=True)
